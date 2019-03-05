699d02d1faab4c4d6d6688582cf5e5435fc1a135
Steffen <steffen.oppel@gmail.com>
2019-03-05 08:10:00 +0000

    revised tripSummary
    modified trip summary to calculate most metrics in one pipe, except for direction to furthest point

--- tripSummary.r
+++ tripSummary.r
@@ -1,8 +1,5 @@
 ## tripSummary   #####################################################################################################################
 
-## MAIN UPDATE: tidyverse, simple features
-
-
 ## STEFFEN OPPEL, 2011
 
 ## this script provides a simple summary for the foraging trips of central place foraging animals
@@ -28,49 +25,39 @@ tripSummary <- function(Trips, Colony=Colony, nests=FALSE)
   if(!"Longitude" %in% names(Colony)) stop("Colony missing Longitude field")
 
 
-
-### SUMMARISE MAX DIST FROM COLONY AND TRIP TRAVELLING TIME FOR EACH TRIP
+  ### SUMMARISE MAX DIST FROM COLONY AND TRIP TRAVELLING TIME FOR EACH TRIP
   
+  ## helper function to calculate distance unless no previous location
+  poss_dist <- possibly(geosphere::distm, otherwise = NA)
   
-  trip_distances <- data.frame(trip=unique(Trips@data$trip_id), max_dist=0, duration=0, total_dist=0) %>%
-    filter(trip!=-1) %>%   ### this removes the non-trip locations
-    mutate(ID=Trips@data$ID[match(trip, Trips@data$trip_id)])
+  ## all summary in one pipe
+  trip_distances <- as.data.frame(Trips@data) %>% 
+    filter(trip_id!=-1) %>%   ### this removes the non-trip locations
+    nest(Longitude, Latitude, .key = "coords") %>%
+    group_by(trip_id) %>%
+    mutate(prev_coords = lag(coords)) %>%
+    ungroup() %>%
+    mutate(Dist = map2_dbl(coords, prev_coords, poss_dist)) %>%
+    mutate(Dist=if_else(is.na(Dist),ColDist,Dist)) %>%
+    mutate(count=1) %>%
+    group_by(ID, trip_id) %>%
+    summarise(n_locs=sum(count),
+              departure=min(DateTime),
+              return=max(DateTime),
+              duration=(max(TrackTime)-min(TrackTime))/3600,
+              total_dist=sum(Dist, na.rm=T)/1000,
+              max_dist=max(ColDist)/1000) %>%
+    mutate(direction=0)
 
-
-for (i in unique(trip_distances$trip)){			### removed as.numeric as this only works with numeric ID
-x<-Trips@data[Trips@data$trip_id==i,]
-maxdist<-cbind(x$Longitude[x$ColDist==max(x$ColDist)],x$Latitude[x$ColDist==max(x$ColDist)])	
-if(dim(maxdist)[1]>1){maxdist<-maxdist[1,]}
-trip_distances[trip_distances$trip==i,2]<-max(Trips@data$ColDist[Trips@data$trip_id==i])/1000
-trip_distances[trip_distances$trip==i,3]<-(max(Trips@data$TrackTime[Trips@data$trip_id==i])-min(Trips@data$TrackTime[Trips@data$trip_id==i]))/3600
-
-
-## Calculate distances from one point to the next and total trip distance
-x$Dist[1]<-x$ColDist[1]/1000				### distance to first point is assumed a straight line from the nest/colony
-for (p in 2:dim(x)[1]){
-p1<-c(x$Longitude[p-1],x$Latitude[p-1])
-p2<-c(x$Longitude[p],x$Latitude[p])
-#x$Dist[p]<-pointDistance(p1,p2, lonlat=T, allpairs=FALSE)/1000			### no longer works in geosphere
-x$Dist[p]<-distMeeus(p1,p2)/1000						### great circle distance according to Meeus, converted to km
-
-}
-trip_distances[trip_distances$trip==i,4]<-sum(x$Dist)+(x$ColDist[p]/1000)				## total trip distance is the sum of all steps plus the dist from the nest of the last location
-
-
-trip_distances$departure[trip_distances$trip==i]<-format(min(x$DateTime),format="%Y-%m-%d %H:%M:%S") 	## departure time of trip
-trip_distances$return[trip_distances$trip==i]<-format(max(x$DateTime),format="%Y-%m-%d %H:%M:%S")		## return time of trip
-trip_distances$n_locs[trip_distances$trip==i]<-dim(x)[1]		## number of locations per trip
-
-
-if(nests == TRUE) {
-origin<- Colony[match(unique(x$ID), Colony$ID),]}
-
-origin<-data.frame(mean(Colony$Longitude), mean(Colony$Latitude)) # CHANGED BY MARIA 8DEC14: was "mean(origin$Latitude)"
-trip_distances$bearing[trip_distances$trip==i]<-bearing(origin,maxdist)			## great circle route bearing of trip
-trip_distances$bearingRhumb[trip_distances$trip==i]<-bearingRhumb(origin,maxdist) 	## constant compass bearing of trip
-
-}
-
-trip_distances<-trip_distances[,c(5,1,6,7,3,2,4,8,9,10)]
+  ### LOOP OVER EACH INDIVIDUAL TRIP TO CALCULATE DIRECTION TO FURTHEST POINT FROM COLONY
+  for (i in unique(trip_distances$trip_id)){			### removed as.numeric as this only works with numeric ID
+    x<-Trips@data[Trips@data$trip_id==i,]
+    maxdist<-cbind(x$Longitude[x$ColDist==max(x$ColDist)],x$Latitude[x$ColDist==max(x$ColDist)])
+    
+    if(nests == TRUE) {origin<- Colony[match(unique(x$ID), Colony$ID),]}else{origin<-Colony}
+    b<-bearing(origin,maxdist)			## great circle route bearing of trip
+    trip_distances$direction[trip_distances$trip_id==i]<-(b + 360) %% 360  ## convert the azimuthal bearing to a compass direction
+    #trip_distances$bearingRhumb[trip_distances$trip_id==i]<-bearingRhumb(origin,maxdist) 	## constant compass bearing of trip
+  }
 return(trip_distances)
 }
