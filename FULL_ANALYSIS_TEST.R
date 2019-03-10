b11274c1da682d1ecbae54e19e832d251c207eb9
Steffen <steffen.oppel@gmail.com>
2019-03-10 17:58:05 +0000

    Revert "updated and included new functions"
    This reverts commit 65b17f6d2e36558f7b6dad933155d674c2ab3c2f.

--- FULL_ANALYSIS_TEST.R
+++ FULL_ANALYSIS_TEST.R
@@ -25,8 +25,8 @@ library(lubridate)
 # LOAD OUR track2iba FUNCTIONS
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-setwd("C:\\STEFFEN\\track2iba")
-#setwd("C:/Users/Martim Bill/Documents/track2iba")
+ # setwd("C:\\STEFFEN\\track2iba")
+setwd("C:/Users/Martim Bill/Documents/track2iba")
 # source("tripSplit.r")
 # source("tripSummary.r")
 # source("scaleARS.r")
@@ -44,15 +44,10 @@ setwd("C:\\STEFFEN\\track2iba")
 source("move2kba.r")
 ### either from Movebank account
 ### working only if password is provided
-#tracks<-move2kba(MovebankID=654043458,User="Steffen",Password="Fr1gateb1rd30")  ## Grey Seal data
-
-## Galapagos tortoises: 174165487
+tracks<-move2kba(MovebankID=114336340,User="Steffen",Password="xxx")
 
 ### or from csv file downloaded from Movebank
-tracks<-move2kba(filename="example_data/GreySeal_McConnell_UK.csv")
-
-### CREATE COLONY DATA FRAME
-Colony<- tracks[1,] %>% dplyr::select(Longitude,Latitude)
+tracks<-move2kba(filename="example_data/MovebankExampleData.csv")
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -82,13 +77,23 @@ tracks <- tracks %>%
 head(tracks)
 
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# DEFINE PROJECTIONS [no longer needed - done within tripSplit]
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# proj.UTM <- CRS(paste("+proj=laea +lon_0=", mean(tracks$Longitude), " +lat_0=", mean(tracks$Latitude), sep=""))
+# DataGroup <- SpatialPointsDataFrame(SpatialPoints(data.frame(tracks$Longitude, tracks$Latitude), proj4string=CRS("+proj=longlat + datum=wgs84")), data = tracks, match.ID=F)
+# DataGroup.Projected <- spTransform(DataGroup, CRS=proj.UTM)
+# plot(DataGroup)
+# points(x=Colony$Longitude,y=Colony$Latitude,type="p",pch=16, col='red')
+
+
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # RUN tripSplit FUNCTION
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 str(tracks)
 source("tripSplit.r")
-Trips<-tripSplit(tracks, Colony=Colony, InnerBuff=5, ReturnBuff=10, Duration=2, plotit=T, nests = F)
+Trips<-tripSplit(tracks, Colony=Colony, InnerBuff=20, ReturnBuff=50, Duration=5, plotit=T, nests = F)
 dim(Trips)
 
 
@@ -108,11 +113,13 @@ dim(trip_distances)
 
 
 
+
+
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # RUN scaleARS FUNCTION
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-source("findScale_dirty.R")
-findH(Trips@data, 
+findH(tracks, 
   ARSscale = T, 
   max_TripDist = pull(trip_distances, "max_dist"), 
   whichStage="Incubation")
@@ -122,15 +129,7 @@ findH(Trips@data,
 # RUN batchUD FUNCTION
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 source("batchUD_clean.r")
-KDE.Surface <- batchUD(DataGroup=Trips[Trips$trip_id != "-1",], Scale = 10, UDLev = 50, polyOut=TRUE, Res=10)
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# RUN THE bootstrap FUNCTION AND ASSIGN THRESHOLD FOR IBA IDENTIFICATION
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-source("bootstrap_new.r")
-repres <- bootstrap(Trips, Scale=5, Iteration=10)
-
+KDE.Surface <- batchUD(DataGroup=Trips[Trips$trip_id != "-1",], Scale = 10, UDLev = 50, polyOut=F, Res=10)
 
 
 
@@ -138,7 +137,7 @@ repres <- bootstrap(Trips, Scale=5, Iteration=10)
 # RUN THE findIBA FUNCTION
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 source("findIBA_clean.r")
-IBAs <- findIBA(KDE.Surface, representativity=0.8, Col.size = 500) ## error here if smoothr not installed!
+IBAs <- findIBA(KDE.Surface, representativity=test_NEW, Col.size = 500) ## error here if smoothr not installed!
 
 
 
@@ -170,5 +169,35 @@ thresh<-ifelse(representativity>0.9,10,ifelse(representativity>0.8,12.5,ifelse(r
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # RUN THE variance Test to assess whether all trips of an individual should be included
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Output <- batchUD(DataGroup, Scale = ScaleOut/2, UDLev = UD)
+
+
+## variance test
+bird_string<-as.character(Output@data$ID)
+vt<-varianceTest(Output, bird_string, Iteration=10)
+vt
+### to choose randomly just one trip per individual
+if (vt < 0.25)
+{
+bird_idtrip=datagroupsUDd@data
+birds=unique(bird_idtrip$originalID)
+trips=numeric()
+set.seed(1)
+for (x in 1:length(birds)) trips=c(trips, as.character(sample(bird_idtrip[bird_idtrip$originalID==(birds[x]),]$ID,1)))
+DataGroupTrips2=DataGroupTrips[DataGroupTrips$ID%in%trips,]
+datagroupsUDd2=batchUD(DataGroupTrips2, Scale=fpt.scales/2, UDLev=50)
+datagroupsUDd=datagroupsUDd2
+DataGroupTrips=DataGroupTrips2
+}
+
+
+
+
+
+
+
+
+
+
 
 
