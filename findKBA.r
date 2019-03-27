eb1113e152127ae6ff8c8162ad0ec1af9be79343
Bealhammar <mbeal@ispa.pt>
2019-03-27 13:19:42 +0000

    findKBA: Readability and map edits
    Spacing in code.
    Changed legend title to reflect percentage vs. number of animals. 
    Small edits to aesthetics of map.

--- findKBA.r
+++ findKBA.r
@@ -43,7 +43,7 @@
 
 
 
-findKBA <- function(KDE.Surface, represent, Col.size = NA, UDLev=50, plotit=TRUE){
+findKBA <- function(KDE.Surface, represent, Col.size = NA, UDLev = 50, plotit = TRUE){
   
   #### LOAD PACKAGES ####
   pkgs <-c('sp', 'sf','smoothr','raster','tidyverse', 'geosphere', 'adehabitatHR')
@@ -57,31 +57,30 @@ findKBA <- function(KDE.Surface, represent, Col.size = NA, UDLev=50, plotit=TRUE
   
 
   #### CALCULATING THRESHOLD OF PROP OF TRACKED ANIMALS NEEDED FROM LASCELLES ET AL. 2016 ####
-  represent<-ifelse(represent>0,represent/100,represent)   ## convert to proportion if people enter percent value
+  represent <- ifelse(represent > 1, represent/100, represent)   ## convert to proportion if people enter percent value
+  
   #threshlkup<-data.frame(rep=c(0.9,0.8,0.7),thresh=c(10,12.5,20),corr=c(0.9,0.75,0.5))
-  if (represent<0.7) warning("UNREPRESENTATIVE SAMPLE: you either did not track a sufficient number of birds to characterise the colony's space use or your species does not lend itself to KBA identification due to its dispersed movement")
-  thresh<-ifelse(represent<=0.7,length(KDE.Surface)*0.5,
-                 ifelse(represent<0.8,length(KDE.Surface)*0.2,
-                        ifelse(represent<0.9,length(KDE.Surface)*0.125,length(KDE.Surface)*0.1))) 
-  corr<-ifelse(represent<=0.7,0.25,
-                 ifelse(represent<0.8,0.5,
-                        ifelse(represent<0.9,0.75,0.9))) 
+  if (represent < 0.7) warning("UNREPRESENTATIVE SAMPLE: you either did not track a sufficient number of birds to characterise the colony's space use or your species does not lend itself to KBA identification due to its dispersed movement")
   
-  
+  thresh <- ifelse(represent <= 0.7,length(KDE.Surface) * 0.5, # length(KDE.Surface) is number of individuals in dataset
+                 ifelse(represent < 0.8, length(KDE.Surface) * 0.2,
+                        ifelse(represent < 0.9,length(KDE.Surface) * 0.125, length(KDE.Surface) * 0.1))) 
+  ## Correction factor: 'correcting' estimates of the proportion of the population in a cell, based on the representativeness of the sample
+  corr <- ifelse(represent <= 0.7, 0.25,
+                 ifelse(represent < 0.8, 0.5,
+                        ifelse(represent < 0.9, 0.75, 0.9))) 
 
   
   ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
   ###### CONVERTING OUTPUT TO PROPORTIONAL UD FOR EACH INDIVIDUAL  ####
   ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
   ## create SpatialPixelsDataFrame
-  #UDLev=50
-  KDEpix <- estUDm2spixdf(KDE.Surface)
-  if(is.projected(KDEpix)!=TRUE) stop("Please re-calculate your kernel UD after projecting the data into a coordinate reference system where units are identical on x- and y-axis")
+  # UDLev=50
+  KDEpix <- adehabitatHR::estUDm2spixdf(KDE.Surface)
+  if(is.projected(KDEpix) != TRUE) stop("Please re-calculate your kernel UD after projecting the data into a coordinate reference system where units are identical on x- and y-axis")
   
   ## calculate area of each pixel
-  pixArea<-KDE.Surface[[1]]@grid@cellsize[1]
-  KDE.Surface<-NULL
-  gc()
+  pixArea <- KDE.Surface[[1]]@grid@cellsize[1]
   ## output reported by kernelUD is intensity / m2
   ## this intensity is multiplied by pixel area (in m2) 
   ## this usage sums to 1 for each individual (some individuals bordering the grid may not sum to 1)
@@ -89,15 +88,15 @@ findKBA <- function(KDE.Surface, represent, Col.size = NA, UDLev=50, plotit=TRUE
   ## to find the 50% UD for an individual, simply use all grid cells where the output value is <0.5
   
   KDEpix@data <- KDEpix@data %>% 
-    mutate(rowname=1:nrow(KDEpix@data)) %>%
-    gather(key="ID",value="UD",-rowname) %>%
-    mutate(usage=UD*(pixArea^2)) %>%
+    mutate(rowname = 1:nrow(KDEpix@data)) %>%
+    gather(key = "ID", value = "UD", -rowname) %>%
+    mutate(usage = UD * (pixArea^2)) %>%
     arrange(ID, desc(usage)) %>%
     group_by(ID) %>%
     mutate(cumulUD = cumsum(usage)) %>%
-    dplyr::select(rowname,ID,cumulUD) %>%
+    dplyr::select(rowname, ID, cumulUD) %>%
     arrange(rowname) %>%
-    spread(key=ID, value=cumulUD) %>%
+    spread(key = ID, value = cumulUD) %>%
     dplyr::select(-rowname)
   
   
@@ -105,75 +104,83 @@ findKBA <- function(KDE.Surface, represent, Col.size = NA, UDLev=50, plotit=TRUE
   #### COUNT THE NUMBER OF OVERLAPPING UD KERNELS ABOVE THE UDLev==50
   ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
   
-  ## convert to a 0/1 pixel depending on whether UDLev=50 was exceeded
-  Noverlaps<-KDEpix
-  Noverlaps@data<-as.data.frame(ifelse(Noverlaps@data<(UDLev/100),1,0)) %>%
+  ## convert pixels to 1 if they are below UDLev and 0 if they are outside this quantile and sums the number of individuals with a '1' in each cell (i.e. number of overlapping individuals)
+  Noverlaps <- KDEpix
+  Noverlaps@data <- as.data.frame(ifelse(Noverlaps@data < (UDLev / 100), 1, 0)) %>%
     mutate(N_IND = rowSums(.)) %>%
     dplyr::select(N_IND)
   
-  KDEpix<-NULL
+  # KDEpix <- NULL
   gc()
   
-  ## CONVERT TO POTENTIAL KBA BASED ON THRESHOLDS
-  potentialKBA<-Noverlaps
-  potentialKBA@data<-potentialKBA@data %>% 
-    mutate(KBA = ifelse(N_IND>= thresh,"potential","no"))
-  if(!is.na(Col.size)){potentialKBA@data$N_animals<- corr*Col.size*(potentialKBA@data$N_IND/length(KDE.Surface))}else{   ## provide the number of ind expected if colony size is given
-    potentialKBA@data$N_animals<- corr*100*(potentialKBA@data$N_IND/length(KDE.Surface))
+  ## Classify each cell as POTENTIAL (KBA) or not based on threshold
+  potentialKBA <- Noverlaps
+  potentialKBA@data <- potentialKBA@data %>% 
+    mutate(KBA = ifelse(N_IND >= thresh, "potential", "no"))
+  ## 'Correct' N animal estimates in cells of POTENTIAL KBA status by the representativeness-set correction factor
+  if(!is.na(Col.size)){
+    potentialKBA@data$N_animals <- corr * Col.size * (potentialKBA@data$N_IND / length(KDE.Surface))
+    }else{   ## provide the number of ind expected if colony size is given
+    potentialKBA@data$N_animals <- corr * 100 * (potentialKBA@data$N_IND / length(KDE.Surface))
     warning("No value for colony size provided. Output for N_animals is in % of colony size")}   ## if no colony size is given then provide output in percent
-  Noverlaps<-NULL
+  KDE.Surface <- NULL
+  Noverlaps <- NULL
+  # gc()
+  
+  ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+  #### CONVERT OUTPUT INTO POLYGONS WITH KBA ASSESSMENT INFO
+  ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    ### the first step is very slow
+  KBApoly <- as(potentialKBA, "SpatialPolygonsDataFrame")
+  # KBApot <- subset(KBApoly, KBA=="potential")
+  #if(dim(KBApoly@data)[1]==0) stop("No areas are used by a sufficient proportion of individuals to qualify as potential KBA.")
+  potentialKBA <- NULL
+  # gc()
+  
+    ### aggregate all pixel-sized polygons into big polygons with the same number of birds 
+  OUTMAP <- aggregate(KBApoly, c('N_animals','N_IND','KBA'))
+  KBApoly<-NULL
+  # gc()
+  
+    ### CONVERT INTO SIMPLE FEATURE AS OUTPUT AND FOR PLOTTING
+  KBA_sf <- st_as_sf(OUTMAP) %>%
+    st_union(by_feature=T) %>%
+    smoothr::smooth(method = "densify") %>%
+    #drop_crumbs(threshold = units::set_units(100, km^2)) %>%
+    #fill_holes(threshold = units::set_units(100, km^2)) %>%
+    st_transform(4326)
+  OUTMAP<-NULL
   gc()
   
-### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-#### CONVERT OUTPUT INTO POLYGONS WITH KBA ASSESSMENT INFO
-### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-  ### the first step is very slow
-KBApoly <- as(potentialKBA, "SpatialPolygonsDataFrame")
-#KBApoly <- subset(KBApoly, KBA=="potential")
-#if(dim(KBApoly@data)[1]==0) stop("No areas are used by a sufficient proportion of individuals to qualify as potential KBA.")
-potentialKBA<-NULL
-gc()
-
-  ### aggregate all pixel-sized polygons into big polygons with the same number of birds 
-OUTMAP <- aggregate(KBApoly, c('N_animals','N_IND','KBA'))
-KBApoly<-NULL
-gc()
-
-  ### CONVERT INTO SIMPLE FEATURE AS OUTPUT AND FOR PLOTTING
-KBA_sf <- st_as_sf(OUTMAP) %>%
-  st_union(by_feature=T) %>%
-  smoothr::smooth(method = "densify") %>%
-  #drop_crumbs(threshold = units::set_units(100, km^2)) %>%
-  #fill_holes(threshold = units::set_units(100, km^2)) %>%
-  st_transform(4326)
-OUTMAP<-NULL
-gc()
-
-
-### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-#### RETURN SIMPLE FEATURE WITH KBA INFO AS OUTPUT AND PLOT
-### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-# ### CREATE MULTIPANEL PLOT OF FORAGING TRIPS WITH INCOMPLETE TRIPS SHOWN AS DASHED LINE
+  
+  ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+  #### RETURN SIMPLE FEATURE WITH KBA INFO AS OUTPUT AND PLOT
+  ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+  # ### CREATE MULTIPANEL PLOT OF FORAGING TRIPS WITH INCOMPLETE TRIPS SHOWN AS DASHED LINE
 
 if(plotit == TRUE) {
-  coordsets<-st_bbox(KBA_sf)
+  coordsets <- st_bbox(KBA_sf)
 
-  KBAPLOT<- KBA_sf %>% dplyr::filter(KBA=="potential") %>%
+  KBAPLOT <- KBA_sf %>% dplyr::filter(KBA=="potential") %>%
     ggplot() +  
     geom_sf(mapping = aes(fill=N_animals), colour="transparent") +
     coord_sf(xlim = c(coordsets$xmin, coordsets$xmax), ylim = c(coordsets$ymin, coordsets$ymax), expand = FALSE) +
-    borders("world", fill="black", colour="black") +
+    borders("world", fill="dark grey", colour="grey20") +
     geom_point(data=Colony, aes(x=Longitude, y=Latitude), col='red', shape=16, size=2) +
     theme(panel.background=element_blank(),
           panel.grid.major=element_line(colour="transparent"),
           panel.grid.minor=element_line(colour="transparent"),
           axis.text=element_text(size=16, color="black"), 
           axis.title=element_text(size=16),
-          panel.border = element_blank()) +
+          panel.border = element_rect(colour = "black", fill=NA, size=1)) +
+    guides(fill = guide_legend(title = "N animals")) +
+    guides(fill = guide_legend(reverse=T)) +
     ylab("Longitude") +
     xlab("Latitude")
-  print(KBAPLOT)
-  
+  if(is.na(Col.size)) { ## make legend title percent
+    KBAPLOT <- KBAPLOT + guides(fill = guide_legend(title = "% of animals"))
+  }
+  print(KBAPLOT)  
 } ## end plotit=T loop
 
 return(KBA_sf)
