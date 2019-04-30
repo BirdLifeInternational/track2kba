a72fcac5d68334590f9698348fd224269ed1135c
Bealhammar <mbeal@ispa.pt>
2019-04-30 10:46:20 +0100

    Bare-bones package; all old files moved to single folder
    Package elements, and one folder containing all old development files the only things in the top-level directory. 
    
    add all_orig_dev_files folder to .Rbuildignore so that it  isn't included in package build
    Small change to .Rbuildignore

---
+++ R/IndEffectTest.R
@@ -0,0 +1,149 @@
+#### IndEffectTest ####
+
+#' Test site fidelity
+#'
+#' \code{IndEffectTest} tests whether the variance in overlap between space use areas within a group (e.g within individuals) is significant compared to between groups (e.g. between individuals).
+#'
+#' This function works by producing kernel density areas at a desired contour level (i.e. \emph{UDLEv}) for each level of \emph{tripID} and estimating the degree of overlap between all pairwise comparisons using the desired overlap \emph{method}. Then, comparisons are split into 'within' and 'between' groups, determined by the grouping variable (i.e \emph{GroupVar}) argument.
+#'
+#' If \emph{conditional=TRUE} then the overlap estimates will range from 0 to \emph{UDLev} (unless \emph{method="HR"}).
+#'
+#' Then, the empirical distribution of each group is compared in a bootstrapped Kolmogorov-Smirnov test, to check whether differences in the distributions are significant. If so, it indicates that individuals within the \emph{GroupVar} reuse sites more than expected by chance.
+#'
+#' NOTE: Because \code{IndEffectTes} relies on \code{\link[adehabitatHR]{kerneloverlap}} to estimate overlap, it was not possible to implement a \emph{Res} argument as is done in other track2KBA functions. Therefore, it is advised to either leave the default of 500 cells, or ascertain the number of cells in the grid of chosen \emph{Res} from the output of \link{estSpaceUse}.
+#'
+#' @param Trips SpatialPointsDataFrame or data.frame. If input is data.frame or unprojected SpatialPointsDF, must include 'Latitude' and 'Longitude' fields.
+#' @param tripID character. Column in \emph{Trips} corresponding to the within group ID (e.g. trip-individual combination)
+#' @param GroupVar character. Column in \emph{Trips} corresponding to the between group ID (e.g. individual or track)
+#' @param plotit logical scalar (TRUE/FALSE). Do you want to output a boxplot of the result?
+#' @param method character. Which method of overlap estimation to use? See \code{\link[adehabitatHR]{kerneloverlap}} for descriptions of each method.
+#' @param conditional logical scalar (T/F). If TRUE, the function sets to 0 the pixels of the grid over which the UD is estimated, outside the home range of the animal estimated at a level of probability equal to percent. Note that this argument has no effect when meth="HR" (from \code{\link[adehabitatHR]{kerneloverlap}}).
+#' @param UDLev numeric. The desired contour level of the utilization distribution to be used in overlap estimation. NOTE: this is irrelevant if \emph{conditional=FALSE}.
+#' @param Scale numeric (in kilometers). Smoothing ('H') parameter for kernel density estimation.
+#' @param grid numeric or SpatialPixels. If numeric, specify the desired number of grid cells over which the utilization distributions will be esimated. A default grid of 500 cells is used.
+#' @param nboots numeric. Indicate the desired number of Kolmogorov-Smirnov iterations to run. 500 is an advisable minimum for statistical rigor.
+#'
+#' @return \code{IndEffectTest} returns a list containing three objects. In the first slot 'Overlap Matrix', the full matrix of overlap comparisons. In the 'Overlap' slot, a dataframe with a column identifying whether each overlap estimate corresponds to a within-group, or a between-group comparison. In the third slot 'Kolmogorov-Smirnov' is the test output of the Kolmogorov-Smirnov test, indicating the D parameter and significance estimates.
+#'
+#' @examples
+#' \dontrun{
+#' indEffect <- IndEffectTest(Trips, GroupVar="ID", tripID="trip_id", method="BA", Scale=HVALS$mag, nboots=500)
+#'
+#' indEffect$`Kolmogorov-Smirnov`}
+#'
+#' @export
+#' @import ggplot2
+#' @import sp
+
+IndEffectTest <- function(Trips, tripID, GroupVar, plotit=T, method = c("HR", "PHR", "VI", "BA", "UDOI", "HD"), conditional = TRUE, UDLev=50, Scale, grid = 500, nboots = 1000)
+{
+
+  # packages
+  # require(sp)
+  # require(adehabitatHR)
+  # require(Matching, quietly = T)
+  # require(tidyverse)
+
+  # initial chceks
+  if (!"Latitude" %in% names(Trips)) stop("Latitude field does not exist")
+  if (!"Longitude" %in% names(Trips)) stop("Longitude field does not exist")
+  if (!(tripID) %in% names(Trips)) stop("Within-group field does not exist")
+  if (!GroupVar %in% names(Trips)) stop("Group field does not exist")
+
+
+  # MB # Added this section which converts Trips to spatial dataframe and projects it (and if already is SPDF it accepts this)
+  if (class(Trips) != "SpatialPointsDataFrame")     ## convert to SpatialPointsDataFrame and project
+  {
+    if (!"Latitude" %in% names(Trips)) stop("Latitude field does not exist")
+    if (!"Longitude" %in% names(Trips)) stop("Longitude field does not exist")
+    ## filter DF to the minimum fields that are needed
+    CleanTrips <- Trips %>%
+      dplyr::select(.data$GroupVar, .data$tripID, .data$Latitude, .data$Longitude)
+    mid_point <- data.frame(geosphere::centroid(cbind(CleanTrips$Longitude, CleanTrips$Latitude)))
+
+    ### PREVENT PROJECTION PROBLEMS FOR DATA SPANNING DATELINE
+    if (min(CleanTrips$Longitude) < -170 &  max(CleanTrips$Longitude) > 170) {
+      longs = ifelse(CleanTrips$Longitude < 0, CleanTrips$Longitude + 360, CleanTrips$Longitude)
+      mid_point$lon <- ifelse(median(longs) > 180, median(longs) - 360, median(longs))}
+
+    Trips.Wgs <- SpatialPoints(data.frame(CleanTrips$Longitude, CleanTrips$Latitude), proj4string = CRS("+proj=longlat + datum=wgs84"))
+    proj.UTM <- CRS(paste("+proj=laea +lon_0=", mid_point$lon, " +lat_0=", mid_point$lat, sep = ""))
+    Trips.Projected <- spTransform(Trips.Wgs, CRS = proj.UTM )
+    TripsSpatial <- SpatialPointsDataFrame(Trips.Projected, data = CleanTrips)
+    TripsSpatial@data <- TripsSpatial@data %>% dplyr::select(GroupVar, tripID, Latitude, Longitude)
+    Trips.Wgs <- NULL
+    Trips.Projected <- NULL
+
+  }else {## if data are already in a SpatialPointsDataFrame then check for projection
+    if (is.projected(Trips)) {
+      if ("trip_id" %in% names(Trips@data)) {
+        TripsSpatial <- Trips }
+      TripsSpatial@data <- TripsSpatial@data %>% dplyr::select(GroupVar, tripID, Latitude, Longitude)
+    }else {## project data to UTM if not projected
+      if (!"Latitude" %in% names(Trips)) stop("Latitude field does not exist")
+      if (!"Longitude" %in% names(Trips)) stop("Longitude field does not exist")
+      mid_point <- data.frame(centroid(cbind(Trips@data$Longitude, Trips@data$Latitude)))
+
+      ### MB  This part prevents projection problems around the DATELINE
+      if (min(Trips@data$Longitude) < -170 &  max(Trips@data$Longitude) > 170) {
+        longs = ifelse(Trips@data$Longitude < 0, Trips@data$Longitude + 360, Trips@data$Longitude)
+        mid_point$lon <- ifelse(median(longs) > 180, median(longs) - 360, median(longs))}
+
+      proj.UTM <- CRS(paste("+proj=laea +lon_0=", mid_point$lon, " +lat_0=", mid_point$lat, sep = ""))
+      TripsSpatial <- spTransform(Trips, CRS = proj.UTM)
+      TripsSpatial@data <- TripsSpatial@data %>% dplyr::select(GroupVar, tripID, Latitude, Longitude)
+    }
+  }
+
+  # remove tripID Trips with < 6 points as they can't be used to calculate kernel
+  # MB edit # Changed this step to happen after SPDF set-up. Also added tripID column.
+  UIDs <- names(which(table(TripsSpatial@data[, tripID]) > 5))
+  TripsSpatial <- TripsSpatial[TripsSpatial@data[, tripID] %in% UIDs, ]
+  TripsSpatial@data[ ,tripID] <- droplevels(as.factor(TripsSpatial@data[ ,tripID]))
+
+  # create vector with value of GroupVar for each trip
+  gid <- TripsSpatial@data[!duplicated(TripsSpatial@data[, tripID]), ][[GroupVar]]
+
+  # calculate overlap between Trips
+  X <- adehabitatHR::kerneloverlap(xy = TripsSpatial[, tripID], method = method, percent = UDLev, conditional = conditional, h = Scale*1000, grid = grid)
+  X[lower.tri(X, diag = T)] <- NA
+
+  # assign value of GroupVar to rows and columns
+  rownames(X) <- colnames(X) <- gid
+
+  # separate within (WI) and between (BW) group overlaps
+  WI <- NULL
+  BW <- NULL
+  for (i in seq_along(rownames(X))) {
+    # i = 1
+    x1 <- X[i,]
+    x2 <- x1[which(names(x1) == rownames(X)[i])]
+    x3 <- x1[which(names(x1) != rownames(X)[i])]
+    WI <- c(WI, x2)
+    BW <- c(BW, x3)
+  }
+  BW <- BW[!is.na(BW)]
+  WI <- WI[!is.na(WI)]
+
+  ## VMP commented this out since the ks.boot function is robust to ties. Was leftover from when using stats::ks.test function
+  # BW <- BW[BW != 0]
+  # WI <- WI[WI != 0]
+
+  # organize values in a dataframe for plotting
+  Overlaps <- data.frame(Overlap = c(WI, BW), Type = c(rep("Within", length(WI)), rep("Between", length(BW))))
+
+  if(plotit==T){
+    print(ggplot(data = Overlaps, aes(x = Type, y = Overlap, fill = Type)) + geom_boxplot() + theme_bw())
+  }
+
+  # ks <- ks.test(x = WI, y = BW)
+  ks <- Matching::ks.boot(WI, BW, alternative = "two.sided", nboots = nboots) # more indicated when data don't come from continuous distr (ours have many 0s)
+
+  # Organise output
+  Result <- list()
+  Result[1] <- list(X) # overlaps matrix
+  Result[2] <- list(Overlaps) # df with overlap values (long format)
+  Result[3] <- list(ks) # output from the ks.boot function
+  names(Result) <- c("Overlap Matrix", "Overlaps", "Kolmogorov-Smirnov")
+  return(Result)
+}
---
+++ R/findKBA.r
@@ -0,0 +1,169 @@
+## findKBA  #####################################################################################################################
+
+#' Delineating areas of aggregation of tracked animals to identify potential Key Areas for Biodiversity (KBA).
+#'
+#' \code{findKBA} uses the utilization distributions of individual animals to identify areas of aggregation (i.e. where a large proportion of individuals' core areas overlap).
+#'
+#' The function first calculates thresholds based on the representativeness of the tracking data (as quantified by \code{\link{repAssess}}). \code{findKBA} then summarises the number of individual core UDs overlapping in an area and compares that number against the thresholds.
+#' The areas identified are POTENTIAL Key Biodiversity Areas. That is, they are areas of ecological relevance to the species, but must yet be assessed against global criteria (conservation status and global population size of the species) to determine whether they achieve global (or regional) KBA status. All criteria are published in the KBA standard, which may be found here: \url{http://www.keybiodiversityareas.org/what-are-kbas}.
+#' @param KDE.Surface estUDm. Either as created by \code{\link{estSpaceUse}} or \code{adehabitatHR::kernelUD}. Only accepted if the utilization distribution was calculated in a projected coordinate reference system.
+#' @param Represent Numeric (between 0-1). Output value provided by \code{\link{repAssess}} which assesses how representative the tracking data are for characterising the space use of the wider population. If this value is <0.7 then a warning will be issued as the data do not meet the representativeness criteria for a KBA.
+#' @param Col.size Numeric, the number of individuals breeding or residing at the origin location from where animals were tracked, quantifying the population that the tracking data represent. This number will be used to calculate how many animals use the delineated areas of aggregation. If no value for \code{Col.size} is provided then output will be as the proportion of the population.
+#' @param UDLev Numeric (percentage). Specifies the quantile used for delineating the core use areas of individuals based on the kernel density distribution. Default set to 50\% based on Lascelles et al. (2016). For penguins higher values can be accepted, see Dias et al. (2018).
+#' @param plotit Logical. If TRUE then a map of identified areas will be drawn.
+#'
+#' @return Returns an object of class \code{sf} containing polygon data with three data columns:
+#'   Column \code{N_IND} indicates the number of tracked individuals whose core use area (at \code{UDLev}) overlapped with this polygon.
+#'
+#'   Column \code{N_animals} estimates the number of animals from the represented population that regularly use the polygon area. If no value for (at \code{Col.size}) was provided, this number is in \% of the size of the represented population.
+#'
+#'   Column \code{KBA} indicates whether the polygon can be considered a potential KBA.
+#'
+#'   If \code{plotit=T}, a map is produced, displaying the areas which hold aggregations above a certain threshold proportion of the population. If there are no areas displayed on the map, then either the species doesn't aggregate, the Scale is too small to identify aggregations in this species, or the tracked sample aren't representative enough to meet the thresholds.
+#'
+#' @examples
+#' \dontrun{
+#' findKBA(KDE.Surface, Represent=Represent$out)
+#' }
+#' @export
+#' @import dplyr
+#' @import ggplot2
+#' @import sf
+
+findKBA <- function(KDE.Surface, Represent, Col.size = NA, UDLev = 50, plotit = TRUE){
+
+  #### LOAD PACKAGES ####
+  # pkgs <- c('sp', 'sf','smoothr','raster','tidyverse', 'geosphere', 'adehabitatHR')
+  # for(p in pkgs) {suppressPackageStartupMessages(require(p, quietly=TRUE, character.only=TRUE, warn.conflicts=FALSE))}
+
+  #### ERROR CHECKING ####
+  if(class(KDE.Surface) %in% c("list")) {KDE.Surface<-KDE.Surface$KDE.Surface}  ### for users who specified polyOut=T in the batchUD function
+  if(!class(KDE.Surface) %in% c("estUDm")) stop("KDE.Surface should be of class 'estUDm' provided by adehabitatHR::kernelUD or track2kba::estSpaceUse")
+  if(length(KDE.Surface) < 10) warning("LOW SAMPLE SIZE: identifying a KBA based on <10 tracked individuals is not recommended")
+
+
+  #### CALCULATING THRESHOLD OF PROP OF TRACKED ANIMALS NEEDED FROM LASCELLES ET AL. 2016 ####
+  Represent <- ifelse(Represent > 1, Represent/100, Represent)   ## convert to proportion if people enter percent value
+
+  #threshlkup<-data.frame(rep=c(0.9,0.8,0.7),thresh=c(10,12.5,20),corr=c(0.9,0.75,0.5))
+  if (Represent < 0.7) warning("UNREPRESENTATIVE SAMPLE: you either did not track a sufficient number of birds to characterise the colony's space use or your species does not lend itself to KBA identification due to its dispersed movement")
+
+  thresh <- ifelse(Represent <= 0.7,length(KDE.Surface) * 0.5, # length(KDE.Surface) is number of individuals in dataset
+                 ifelse(Represent < 0.8, length(KDE.Surface) * 0.2,
+                        ifelse(Represent < 0.9,length(KDE.Surface) * 0.125, length(KDE.Surface) * 0.1)))
+  ## Correction factor: 'correcting' estimates of the proportion of the population in a cell, based on the representativeness of the sample
+  corr <- ifelse(Represent <= 0.7, 0.25,
+                 ifelse(Represent < 0.8, 0.5,
+                        ifelse(Represent < 0.9, 0.75, 0.9)))
+
+  ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+  ###### CONVERTING OUTPUT TO PROPORTIONAL UD FOR EACH INDIVIDUAL  ####
+  ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+  ## create SpatialPixelsDataFrame
+  # UDLev=50
+  KDEpix <- adehabitatHR::estUDm2spixdf(KDE.Surface)
+  if(sp::is.projected(KDEpix) != TRUE) stop("Please re-calculate your kernel UD after projecting the data into a coordinate reference system where units are identical on x- and y-axis")
+
+  ## calculate area of each pixel
+  pixArea <- KDE.Surface[[1]]@grid@cellsize[1]
+  ## output reported by kernelUD is intensity / m2
+  ## this intensity is multiplied by pixel area (in m2)
+  ## this usage sums to 1 for each individual (some individuals bordering the grid may not sum to 1)
+  ## we sort this usage and calculate the cumulative sum -> this is effectively the "% UD"
+  ## to find the 50% UD for an individual, simply use all grid cells where the output value is <0.5
+
+  KDEpix@data <- KDEpix@data %>%
+    mutate(rowname = 1:nrow(KDEpix@data)) %>%
+    tidyr::gather(key = "ID", value = "UD", -.data$rowname) %>%
+    mutate(usage = .data$UD * (pixArea^2)) %>%
+    arrange(.data$ID, desc(.data$usage)) %>%
+    group_by(.data$ID) %>%
+    mutate(cumulUD = cumsum(.data$usage)) %>%
+    dplyr::select(.data$rowname, .data$ID, .data$cumulUD) %>%
+    arrange(.data$rowname) %>%
+    tidyr::spread(key = .data$ID, value = .data$cumulUD) %>%
+    dplyr::select(-.data$rowname)
+
+
+  ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+  #### COUNT THE NUMBER OF OVERLAPPING UD KERNELS ABOVE THE UDLev==50
+  ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+
+  ## convert pixels to 1 if they are below UDLev and 0 if they are outside this quantile and sums the number of individuals with a '1' in each cell (i.e. number of overlapping individuals)
+  Noverlaps <- KDEpix
+  Noverlaps@data <- as.data.frame(ifelse(Noverlaps@data < (UDLev / 100), 1, 0)) %>%
+    mutate(N_IND = rowSums(.)) %>%
+    dplyr::select(.data$N_IND)
+
+  # KDEpix <- NULL
+
+  ## Classify each cell as POTENTIAL (KBA) or not based on threshold
+  potentialKBA <- Noverlaps
+  potentialKBA@data <- potentialKBA@data %>%
+    mutate(KBA = ifelse(.data$N_IND >= thresh, "potential", "no"))
+  ## 'Correct' N animal estimates in cells of POTENTIAL KBA status by the representativeness-set correction factor
+  if(!is.na(Col.size)){
+    potentialKBA@data$N_animals <- corr * Col.size * (potentialKBA@data$N_IND / length(KDE.Surface))
+    }else{   ## provide the number of ind expected if colony size is given
+    potentialKBA@data$N_animals <- corr * 100 * (potentialKBA@data$N_IND / length(KDE.Surface))
+    warning("No value for colony size provided. Output for N_animals is in % of colony size")}   ## if no colony size is given then provide output in percent
+  # KDE.Surface <- NULL
+  Noverlaps <- NULL
+
+  ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+  #### CONVERT OUTPUT INTO POLYGONS WITH KBA ASSESSMENT INFO
+  ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    ### the first step is very slow
+  KBApoly <- as(potentialKBA, "SpatialPolygonsDataFrame")
+  # KBApot <- subset(KBApoly, KBA=="potential")
+  #if(dim(KBApoly@data)[1]==0) stop("No areas are used by a sufficient proportion of individuals to qualify as potential KBA.")
+  potentialKBA <- NULL
+
+    ### aggregate all pixel-sized polygons into big polygons with the same number of birds
+  OUTMAP <- aggregate(KBApoly, c('N_animals','N_IND','KBA'))
+  KBApoly <- NULL
+
+    ### CONVERT INTO SIMPLE FEATURE AS OUTPUT AND FOR PLOTTING
+  KBA_sf <- sf::st_as_sf(OUTMAP) %>%
+    sf::st_union(by_feature = TRUE) %>%
+    smoothr::smooth(method = "densify") %>%
+    #drop_crumbs(threshold = units::set_units(100, km^2)) %>%
+    #fill_holes(threshold = units::set_units(100, km^2)) %>%
+    sf::st_transform(4326)
+  OUTMAP <- NULL
+
+  ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+  #### RETURN SIMPLE FEATURE WITH KBA INFO AS OUTPUT AND PLOT
+  ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+  # ### CREATE MULTIPANEL PLOT OF FORAGING TRIPS WITH INCOMPLETE TRIPS SHOWN AS DASHED LINE
+
+if(plotit == TRUE) {
+  coordsets <- sf::st_bbox(KBA_sf)
+
+  KBAPLOT <- KBA_sf %>% dplyr::filter(.data$KBA=="potential") %>%
+    ggplot() +
+    geom_sf(mapping = aes(fill=N_animals), colour="transparent") +
+    coord_sf(xlim = c(coordsets$xmin, coordsets$xmax), ylim = c(coordsets$ymin, coordsets$ymax), expand = FALSE) +
+    borders("world", fill="dark grey", colour="grey20") +
+    geom_point(data=Colony, aes(x=Longitude, y=Latitude), col='red', shape=16, size=2) +
+    theme(panel.background=element_blank(),
+      panel.grid.major=element_line(colour="transparent"),
+      panel.grid.minor=element_line(colour="transparent"),
+      axis.text=element_text(size=16, color="black"),
+      axis.title=element_text(size=16),
+      panel.border = element_rect(colour = "black", fill=NA, size=1)) +
+    scale_fill_continuous(name = "N animals") +
+    ylab("Longitude") +
+    xlab("Latitude")
+  if(is.na(Col.size)) { ## make legend title percent
+    KBAPLOT <- KBAPLOT + scale_fill_continuous(name = "% of animals")
+  }
+  print(KBAPLOT)
+} ## end plotit=T loop
+
+return(KBA_sf)
+
+} ### end findKBA function loop
+
+
+
