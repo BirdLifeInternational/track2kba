f99e4cc695f7d3572384d0d76c322d621b6c5a02
Bealhammar <mbeal@ispa.pt>
2020-08-06 10:04:47 +0100

    mapTrips: user can select IDs to plot
    enter 'IDs' argument

--- R/mapTrips.R
+++ R/mapTrips.R
@@ -14,81 +14,98 @@
 #' specifying the central location(s) from which trips begin. If more than one 
 #' location, each row should correspond to an appropriate location (Lat/Lon) for
 #'  each ID value in \emph{trips}.
+#' @param IDs numeric vector. Sequence of numeric indices for the IDs you wish
+#' to map. Max of 25. 
 #' @return Returns a figure of facetted maps, each of which corresponds to a 
 #' level of ID in \emph{trips}.
 #'
 #' @seealso \code{\link{tripSplit}}
 #'
 #' @examples
-#' \dontrun{trips <- mapTrips(trips, colony)}
+#' \dontrun{
+#' trips <- mapTrips(trips, colony)
+#' trips <- mapTrips(trips, colony, IDs = 2:10) # show IDs  #2 through #10
+#' }
 #' @export
 #' @importFrom ggplot2 ggplot aes scale_x_continuous geom_path geom_point
 #' @importFrom ggplot2 facet_wrap vars theme element_rect element_blank
 #' @importFrom rlang .data
 
-mapTrips <- function(trips, colony){
-
-  if(length(unique(trips@data$ID))>25){
-    selectIDs <- unique(trips@data$ID)[1:25]
-    plotdat <- trips@data %>% dplyr::filter(.data$ID %in% selectIDs)
-    message("Too many individuals to plot. Only first 25 ID's will be shown")
-  } else {plotdat <- trips@data}
-  TRACKPLOT <- plotdat %>% 
-    dplyr::mutate(complete=ifelse(.data$Returns=="No","No","Yes")) %>%
-    dplyr::arrange(.data$ID, .data$DateTime) %>%
-    ggplot(
-      aes(.data$., x=.data$Longitude, y=.data$Latitude, col=.data$complete)
-      ) +
-    geom_path() +
-    geom_point(
-      data=colony, aes(x=.data$Longitude, y=.data$Latitude), 
-      fill='dark orange', color='black', pch=21, size=2
-    ) +
-    facet_wrap(~ID) +
-    theme(panel.background=element_rect(fill="white", colour="black"),
-      panel.grid.major = element_blank(),
-      panel.grid.minor = element_blank(),
-      strip.background = element_rect(colour="black", fill="white"),
-      panel.border = element_blank())
+mapTrips <- function(trips, colony, IDs=NULL){
+  
+  if(is.null(IDs)){
+    IDs <- 1:dplyr::n_distinct(trips@data$ID)
+  }
+  
+  N_ceiling <- ifelse(
+    n_distinct(trips@data$ID) > 25, 25, n_distinct(trips@data$ID)
+    )
+  
+  if(length(IDs) > N_ceiling) {
+    IDs <- IDs[1:N_ceiling]
+    message("Too many individuals to plot. Only 25 IDs will be shown.")
+  }
+  
+  selectIDs <- unique(trips@data$ID)[IDs]
+  plotdat <- trips@data %>% dplyr::filter(.data$ID %in% selectIDs)
   
   ##### DIFFERENT PLOT FOR BIRDS CROSSING THE DATELINE ------------------------
-  if(min(trips$Longitude) < -170 &  max(trips$Longitude) > 170) {
-    plotdat <-  trips@data %>%
+  if(min(plotdat$Longitude) < -170 & max(plotdat$Longitude) > 170) {
+    plotdat <- plotdat@data %>%
       mutate(
         Longitude=ifelse(
           .data$Longitude<0, .data$Longitude+360, .data$Longitude
-          )
         )
+      )
     
     colony$Longitude  <- ifelse(
       colony$Longitude<0, colony$Longitude+360, colony$Longitude
-      )
+    )
     
     longlimits <- c(min(plotdat$Longitude) - 2, max((plotdat$Longitude) + 2))
     longbreaks <- round(seq(longlimits[1], longlimits[2], by=10) / 10, 0) * 10
     longlabels <- ifelse(longbreaks > 180, longbreaks - 360, longbreaks)
-
+    
     TRACKPLOT <- plotdat %>% 
       mutate(complete = ifelse(.data$Returns=="No", "No", "Yes")) %>%
       arrange(.data$ID, .data$DateTime) %>% 
       ggplot( 
-        aes(.data$., x = .data$Longitude, y = .data$Latitude, col = .data$complete)
-        ) +
+        aes(.data$., x = .data$Longitude, y = .data$Latitude, 
+            col = .data$complete)
+      ) +
       geom_path() +
       geom_point(data = colony, 
-        aes(x=.data$Longitude, y=.data$Latitude), 
-        fill='dark orange', color='black', pch=21, size=2
-        ) +
+                 aes(x=.data$Longitude, y=.data$Latitude), 
+                 fill='dark orange', color='black', pch=21, size=2
+      ) +
       scale_x_continuous(limits = longlimits,
-        breaks = longbreaks,
-        labels = longlabels) +
+                         breaks = longbreaks,
+                         labels = longlabels) +
       facet_wrap(~.data$ID)  +
       theme(panel.background=element_rect(fill="white", colour="black"),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        strip.background = element_rect(colour="black", fill="white"),
-        panel.border = element_blank())
-    }
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            strip.background = element_rect(colour="black", fill="white"),
+            panel.border = element_blank())
+  } else {
+    TRACKPLOT <- plotdat %>% 
+      dplyr::mutate(complete=ifelse(.data$Returns=="No","No","Yes")) %>%
+      dplyr::arrange(.data$ID, .data$DateTime) %>%
+      ggplot(
+        aes(.data$., x=.data$Longitude, y=.data$Latitude, col=.data$complete)
+      ) +
+      geom_path() +
+      geom_point(
+        data=colony, aes(x=.data$Longitude, y=.data$Latitude), 
+        fill='dark orange', color='black', pch=21, size=2
+      ) +
+      facet_wrap(~ID) +
+      theme(panel.background=element_rect(fill="white", colour="black"),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            strip.background = element_rect(colour="black", fill="white"),
+            panel.border = element_blank())
+  }
   
   base::print(TRACKPLOT)
   
